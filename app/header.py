101858eebb73f79710bb85f3e83aeb78d35fb9b2
maxkamrani <115466232+maxkamrani@users.noreply.github.com>
2023-09-13 21:26:49 +0200

    Create header.py

---
+++ app/header.py
@@ -0,0 +1,13 @@
+import streamlit as st
+from langchain.vectorstores import FAISS
+from langchain.embeddings.openai import OpenAIEmbeddings
+import os 
+import pandas as pd
+from langchain.document_loaders import DataFrameLoader
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from langchain.chat_models import ChatOpenAI
+from htmlTemplates import css, bot_template, user_template
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+os.environ['OPENAI_API_KEY'] =''
