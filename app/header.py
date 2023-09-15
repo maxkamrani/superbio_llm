1165feb41e58f0ae6ea0ad54243844b018c29e31
maxkamrani <115466232+maxkamrani@users.noreply.github.com>
2023-09-15 13:22:34 +0200

    Update header.py

--- app/header.py
+++ app/header.py
@@ -1,4 +1,3 @@
-#to be removed 
 import streamlit as st
 from langchain.vectorstores import FAISS
 from langchain.embeddings.openai import OpenAIEmbeddings
@@ -9,3 +8,6 @@ from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from langchain.chat_models import ChatOpenAI
 from htmlTemplates import css, bot_template, user_template
+
+
+os.environ['OPENAI_API_KEY'] ='sk-XXXX'
